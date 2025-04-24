9a5410117536549286352048905724ec0e06cf58
coldplugin <coldplugin@gmail.com>
2025-04-24 08:04:49 +0200

    Add files via upload

--- hitag2_crypt.c
+++ hitag2_crypt.c
@@ -1,7 +1,7 @@
 #include "hitag2_crypt.h"
 
-uint64 hitag2_reverse(uint64 old, int num) {
-    uint64 reversed;
+uint64_t hitag2_reverse(uint64_t old, int num) {
+    uint64_t reversed;
     int i;
 
     reversed = 0;
@@ -12,15 +12,15 @@ uint64 hitag2_reverse(uint64 old, int num) {
 
     return reversed;
 }
-uint16 hitag2_makeWord(uint64 x, int a, int b, int c, int d) {
-	uint16 wrd;
+uint16_t hitag2_makeWord(uint64_t x, int a, int b, int c, int d) {
+	uint16_t wrd;
 
 	wrd = ((x >> a)&1) | (((x >> b)&1) << 1) | (((x >> c)&1) << 2) | (((x >> d)&1) << 3);
 	return wrd;
 }
 
-uint32 hitag2_hash(uint64 x) {
-	uint32 tmp;
+uint32_t hitag2_hash(uint64_t x) {
+	uint32_t tmp;
 	tmp =
 		((0x2C79 >> hitag2_makeWord(x, 1, 2, 4, 5)) & 1) |
         (((0x6671 >> hitag2_makeWord(x, 7, 11, 13, 14)) & 1) << 1) |
@@ -31,35 +31,35 @@ uint32 hitag2_hash(uint64 x) {
 	return tmp;
 }
 
-byte hitag2_bit(uint64 * sd) {
-	uint64 x = *sd;
+uint8_t hitag2_bit(uint64_t * sd) {
+	uint64_t x = *sd;
 	x = (x >> 1) | (((x ^ (x >> 2) ^ (x >> 3) ^ (x >> 6) ^ (x >> 7) ^ (x >> 8) ^ (x >> 16) ^ (x >> 22) ^
 	(x >> 23) ^ (x >> 26) ^(x >> 30) ^ (x >> 41) ^ (x >> 42) ^ (x >> 43) ^ (x >> 46) ^ (x >> 47)) & 1) << 47);
 	*sd = x;
 	return hitag2_hash(x);
 }
 
-byte hitag2_byte(uint64 *sd) {
-	byte newByte;
+uint8_t hitag2_uint8_t(uint64_t *sd) {
+	uint8_t newuint8_t;
 	int i;
 
-	newByte = 0;
+	newuint8_t = 0;
 	for(i=0; i<8; i++) {
-		newByte |= hitag2_bit(sd) << (7-i);
+		newuint8_t |= hitag2_bit(sd) << (7-i);
 	}
 
-	return newByte;
+	return newuint8_t;
 }
-void hitag2_crypt(byte * data, byte * result, int bits, uint64 * sd) {
-	int bytes;
+void hitag2_crypt(uint8_t * data, uint8_t * result, int bits, uint64_t * sd) {
+	int uint8_ts;
 	int rbits;
 	int i;
 	int j;
 
-	bytes = bits >> 3;
+	uint8_ts = bits >> 3;
 	rbits = bits & 0x7;
-	for(i=0; i < bytes; i++)
-		result[i] = data[i] ^ hitag2_byte(sd);
+	for(i=0; i < uint8_ts; i++)
+		result[i] = data[i] ^ hitag2_uint8_t(sd);
 	if(rbits) {
 		result[i] = 0;
 		for(j=0; j<rbits; j++)
@@ -67,7 +67,7 @@ void hitag2_crypt(byte * data, byte * result, int bits, uint64 * sd) {
 	}
 }
 
-void hitag2_init(byte reversing, uint32 iskLow, uint16 iskHi, uint32 random, uint32 cardId, uint64 * seed) {
+void hitag2_init(uint8_t reversing, uint32_t iskLow, uint16_t iskHi, uint32_t random, uint32_t cardId, uint64_t * seed) {
 	int i;
 
 	if(reversing) {
@@ -77,9 +77,9 @@ void hitag2_init(byte reversing, uint32 iskLow, uint16 iskHi, uint32 random, uin
 		cardId = hitag2_reverse(cardId, 32);
 	}
 	
-	*seed = ((uint64)iskHi << 32) | cardId;
+	*seed = ((uint64_t)iskHi << 32) | cardId;
 	for(i = 0; i < 32; i++) {
 		*seed >>= 1;
-		*seed |= (uint64)(hitag2_hash(*seed) ^ ((random >> i) & 1) ^ ((iskLow >> i) & 1)) << 47;
+		*seed |= (uint64_t)(hitag2_hash(*seed) ^ ((random >> i) & 1) ^ ((iskLow >> i) & 1)) << 47;
 	}
 }
--- hitag2_crypt.h
+++ hitag2_crypt.h
@@ -1,12 +1,9 @@
-typedef unsigned char byte;
-typedef unsigned short uint16;
-typedef unsigned int uint32;
-typedef unsigned long long uint64;
+#include <stdint.h>
 
-hitag2_reverse(uint64 old, int num);
-uint16 hitag2_makeWord(uint64 x, int a, int b, int c, int d);
-uint32 hitag2_hash(uint64 x);
-byte hitag2_bit(uint64 * sd);
-byte hitag2_byte(uint64 *sd);
-void hitag2_crypt(byte * data, byte * result, int bits, uint64 * sd);
-void hitag2_init(byte reversing, uint32 iskLow, uint16 iskHi, uint32 random, uint32 cardId, uint64 * seed);
+hitag2_reverse(uint64_t old, int num);
+uint16_t hitag2_makeWord(uint64_t x, int a, int b, int c, int d);
+uint32_t hitag2_hash(uint64_t x);
+uint8_t hitag2_bit(uint64_t * sd);
+uint8_t hitag2_uint8_t(uint64_t *sd);
+void hitag2_crypt(uint8_t * data, uint8_t * result, int bits, uint64_t * sd);
+void hitag2_init(uint8_t reversing, uint32_t iskLow, uint16_t iskHi, uint32_t random, uint32_t cardId, uint64_t * seed);
